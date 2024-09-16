b19f0ba5d1dac5eef428d676162e9c19eab84df7
fedisaev <127963492+Fed0102@users.noreply.github.com>
2024-09-16 11:30:50 +0300

    добавление ссылок на пункты меню

--- scripts/lang.js
+++ scripts/lang.js
@@ -7,35 +7,13 @@ function switchLanguage(lang) {
 
     const svgRu = document.querySelector('[data-lang-svg-ru]');
     const svgEn = document.querySelector('[data-lang-svg-en]');
-    const svgRuMob = document.querySelector('[data-lang-svg-ru-mob]');
-    const svgEnMob = document.querySelector('[data-lang-svg-en-mob]');
 
-    const screenWidth = window.innerWidth;
-
-    if (screenWidth > 600) {
-        if (lang === 'ru') {
-            svgRu.style.display = 'block';
-            svgEn.style.display = 'none';
-            svgRuMob.style.display = 'none';
-            svgEnMob.style.display = 'none';
-        } else if (lang === 'en') {
-            svgRu.style.display = 'none';
-            svgEn.style.display = 'block';
-            svgRuMob.style.display = 'none';
-            svgEnMob.style.display = 'none';
-        }
-    } else {
-        if (lang === 'ru') {
-            svgRu.style.display = 'none';
-            svgEn.style.display = 'none';
-            svgRuMob.style.display = 'flex';
-            svgEnMob.style.display = 'none';
-        } else if (lang === 'en') {
-            svgRu.style.display = 'none';
-            svgEn.style.display = 'none';
-            svgRuMob.style.display = 'none';
-            svgEnMob.style.display = 'block';
-        }
+    if (lang === 'ru') {
+        svgRu.style.display = 'block';
+        svgEn.style.display = 'none';
+    } else if (lang === 'en') {
+        svgRu.style.display = 'none';
+        svgEn.style.display = 'block';
     }
 }
 
@@ -56,12 +34,4 @@ langSwitchers.forEach(button => {
 
 document.addEventListener('DOMContentLoaded', () => {
     switchLanguage('ru');
-
-    window.addEventListener('resize', () => {
-        const activeButton = document.querySelector('.dafes-switcher.active');
-        if (activeButton) {
-            const selectedLang = activeButton.getAttribute('data-lang');
-            switchLanguage(selectedLang);
-        }
-    });
 });
